938b9672a0d85291cc5e9b4924ca98abf8d81b44
mbreslawskij <mbreslawskij@gmail.com>
2021-06-25 20:34:19 -0300

    commit

--- js/saudacao.js
+++ js/saudacao.js
@@ -1,6 +1,11 @@
 const formulario = document.getElementById('formulario');
 const nome = document.getElementById('nome_usuario');
 const sobrenome = document.getElementById('sobrenome_usuario');
+const retorno = document.getElementById('retorno');
+
+const primeiroH1 = document.querySelector('h1');
+primeiroH1.style.fontFamily = 'verdana';
+primeiroH1.style.color = 'green';
 
 let texto = "Olá" + nome.value + "seja bem vindo";
 
@@ -10,4 +15,10 @@ formulario.onsubmit = function (event){
     console.log(nome.value);
     console.log(sobrenome.value);
 
+    const texto = "Oi " + nome.value + " " + sobrenome.value + ", seja(a) bem vindo!!!";
+
+    retorno.innerHTML = texto;
+
+
 }
+
