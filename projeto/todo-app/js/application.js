4d5a49c3a9507fbc001ff8a409831057fbc53222
mbreslawskij <mbreslawskij@gmail.com>
2021-06-30 23:05:44 -0300

    Finalizacao projeto

--- projeto/todo-app/js/application.js
+++ projeto/todo-app/js/application.js
@@ -5,27 +5,67 @@ const tarefas = document.getElementById('tarefas');
 
 let contador = 0;
 
-tarefaForm.addEventListener('submit', function (event){
-    event.preventDefault();
-    contador++;
-    const tarefaTexto = `
-    <li class="list-group-item">
-    <div class="form-check">
-        <input type="checkbox" id="tarefa-${contador}" class="form-check-input">
-        <label for="tarefa-${contador}" class="form-check-label d-block">
-            ${tarefaInput.value}
-        </label>
-    </div>
-    </li>
-    `;
-    
-    console.log(tarefaInput.value);
-    tarefas.innerHTML += tarefaTexto;
-    tarefaInput.value = '';
-
+fetch('https://jsonplaceholder.typicode.com/todos')
+//neste momento convertemos json em objeto/array em javascript
+.then(response => response.json())
+.then(json => {
+    json.forEach(todo => addTarefa(todo.completed, todo.title));
 });
 
+tarefaForm.addEventListener('submit', function (event){
+    event.preventDefault();
+
+    if(tarefaInput.value.length <=3){
+        tarefaInput.classList.add('is-invalid');
+        console.log('Caiu no IF');
+        tarefaInput.focus();
+
+        const feedback = tarefaInput.parentNode.querySelector('.invalid-feedback');
+        const totalDigitado = tarefaInput.value.length;
+
+        feedback.innerText = `Você Digitou  ${totalDigitado} , favor digitar 4 ou mais caracteres.`;
+
+        return false;
+    }
+    tarefaInput.classList.remove('is-invalid');
+
+        addTarefa(false, tarefaInput.value);
+
+   
+});
+
+function addTarefa(status,tarefaDigitada){
+    contador++;
+
+    const checkedString = status ? 'checked' : '';
+
+    const tarefaTexto = `
+    <li class="list-group-item">
+                        <div class="form-check d-flex justify-content.betweem align-items-center">
+                            <input type="checkbox" id ="tarefa=${contador}" class="form-check-input" ${checkedString}>
+                            <label for="tarefa-${contador}" class="form-check-label flex-grow-1 ms-3">
+                                ${tarefaDigitada} 
+                            </label>
+                            <button class="btn btn-danger" onclick="deletar(this);">Deletar</button>
+                        </div>
+
+                    </li>
+    `;
+    
+    tarefas.innerHTML += tarefaTexto;
+}
+
 concluidoTarefa.onclick = () =>{
-    const checks = document.querySelectorAll('#tarefas imput:checked');
-    console.log();
+    // querySelector está pegando todos os input marcado como checked
+    const checks = document.querySelectorAll('#tarefas input:checked');
+    //elemento.remove()
+    // console.log();
+    //estamos andando sobre os elementos dentro da variavel - checks
+    checks.forEach(elemento => {
+        elemento.parentNode.parentNode.remove();
+
+    });
+}
+function deletar(elemento) {
+    elemento.parentNode.parentNode.remove();
 }
